eedb9c5b57c90a5f438392f3a04dfc94927d5855
Timo <jootimo@gmail.com>
2023-04-18 20:45:19 +0300

    ex2.16-2.17

--- part2/puhelinluettelo/src/App.js
+++ part2/puhelinluettelo/src/App.js
@@ -2,11 +2,27 @@ import { useState, useEffect } from 'react'
 import personService from './services/Persons'
 
 const Contact = ({person, onDelete}) => {
+  const style = {
+    color: 'grey',
+    paddingTop: 5,
+    fontSize: 15
+  }
   return (
-    <li> 
+    <li style={style}> 
       {person.name} {person.number} 
       <button onClick= {onDelete}>delete</button>
     </li>
+  )
+}
+
+const Notification = ({notification}) => {
+  if(notification === null){
+    return null 
+  }
+  return (
+    <div style={notification.style}>
+      {notification.message}
+    </div>
   )
 }
 
@@ -41,6 +57,41 @@ const App = () => {
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
+  const [notification, setNotification] = useState(null)
+
+  const setNotificationMessage = (msg, stl) => {
+    if(msg === null)  {
+      setNotification(null)
+    }
+    setNotification({message: msg, style: stl})
+    setTimeout(() => setNotification(null), 5000)
+  }
+
+  const setErrorMessage = (message) => {
+    const errorStyle = {
+      color: 'red',
+      background: 'lightgrey',
+      fontSize: 20,
+      borderStyle: 'solid',
+      borderRadius: 5,
+      padding: 10,
+      marginBottom: 10
+    }
+    setNotificationMessage(message, errorStyle)
+  }
+
+  const setInfoMessage = (message) => {
+    const infoStyle = {
+      color: 'green',
+      background: 'lightgrey',
+      fontSize: 20,
+      borderStyle: 'solid',
+      borderRadius: 5,
+      padding: 10,
+      marginBottom: 10
+    }
+    setNotificationMessage(message, infoStyle)
+  }
 
   const fillPersonList = () => {
     personService.getAll()
@@ -48,7 +99,7 @@ const App = () => {
           console.log('Got persons from server:', allPersons)
           setPersons(allPersons)
       })
-      .catch(error => alert('failed to get persons from server:', error))
+      .catch(error => console.log('failed to get persons from server:', error))
   }
   useEffect(fillPersonList, [])
 
@@ -63,11 +114,12 @@ const App = () => {
         personService.update(updatedPerson)
           .then(updateResponse => {
             console.log('person updated: ', updateResponse)
+            setInfoMessage(`Updated ${updateResponse.name}`)
             setPersons(persons.map(p => p.id === updateResponse.id ? updateResponse : p)) 
             setNewName('')
             setNewNumber('')
           })
-          .catch(error => alert(`failed to update person ${updatedPerson}:`, error))
+          .catch(error => setErrorMessage(`Failed to update ${updatedPerson.name}:`))
       }
       return
     }
@@ -80,11 +132,12 @@ const App = () => {
     personService.create(newPerson)
       .then(createdPerson => {
         console.log('Created a new person on the server:', createdPerson)
+        setInfoMessage(`Created ${createdPerson.name}`)
         setPersons(persons.concat(createdPerson))
         setNewName('')
         setNewNumber('')
       })
-      .catch(error => alert(`failed to create new person ${newPerson}:`, error))
+      .catch(error => setErrorMessage(`Failed to create ${newPerson.name}`))
   }
 
   const onNameInputChange = (event) => {
@@ -102,8 +155,14 @@ const App = () => {
     if(window.confirm(`Delete ${person.name}?`)) {
         console.log('deleting person:', person);
         personService.remove(person.id)
-          .then(() => fillPersonList())
-          .catch(error => alert(`failed to delete person ${person}:`, error))
+          .then(() => {
+            setInfoMessage(`Deleted ${person.name}`)
+            fillPersonList()
+          }) 
+          .catch(error => {
+            setErrorMessage(`Information of ${person.name} has already been removed from the server.`)
+            fillPersonList()
+          })       
     }
   }
 
@@ -114,6 +173,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification notification={notification}></Notification>
       <FilterForm onFilterChange={onFilterChange}/>
       <h2>Add a new contact</h2>
       <NewContactForm 
